059b398b0d3fefc63d33be390f5166bc56d7b974
xborismenx <boris.tikhonov.21@gmail.com>
2025-12-21 22:52:28 +0200

    Add Redis configuration to settings

--- config/settings.py
+++ config/settings.py
@@ -2,10 +2,13 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+
     POSTGRES_USER: str
     POSTGRES_PASSWORD: str
     POSTGRES_HOST: str
     POSTGRES_DB_PORT: int = 5432
     POSTGRES_DB: str = "circuit_breaker_db"
 
-    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+    REDIS_HOST: str = "localhost"
+    REDIS_PORT: int = 6379
