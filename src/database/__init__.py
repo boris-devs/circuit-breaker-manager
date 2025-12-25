adebaa039a72bf63b8356da341356a3e7a6a21f2
xborismenx <boris.tikhonov.21@gmail.com>
2025-12-25 23:31:09 +0200

    Fix incorrect import path in database initialization module

--- src/database/__init__.py
+++ src/database/__init__.py
@@ -1,2 +1,2 @@
 from src.database.models.base import Base
-from database.session_postgresql import get_postgresql_db as get_db
+from src.database.session_postgresql import get_postgresql_db as get_db
