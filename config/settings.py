8e0b9fe1933a70fece9259f6220b458e87ec990f
xborismenx <boris.tikhonov.21@gmail.com>
2025-12-25 23:29:48 +0200

    Add `TIMEOUT_CHECK_SERVICES` setting to configure service check timeout

--- config/settings.py
+++ config/settings.py
@@ -4,6 +4,8 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
+    TIMEOUT_CHECK_SERVICES: int = 15
+
     POSTGRES_USER: str
     POSTGRES_PASSWORD: str
     POSTGRES_HOST: str
