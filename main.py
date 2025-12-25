b04631a4b9dcde7afa6580321a88c5bb13b60058
xborismenx <boris.tikhonov.21@gmail.com>
2025-12-25 23:35:02 +0200

    Integrate Redis Queue dashboard, RQ scheduler, and asynchronous lifespan management for service monitoring

--- main.py
+++ main.py
@@ -1,18 +1,38 @@
-from fastapi import FastAPI
-from views import service_monitoring_router
+import asyncio
+from datetime import datetime, timezone
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+from fastapi import FastAPI
+from rq_dashboard_fast import RedisQueueDashboard
+
+from config.settings import Settings
+from tasks.tasks import run_all_monitoring_checks
+from views import service_monitoring_router
+from sheduler.rq_sheduler import scheduler, queue
+
+settings = Settings()
+
+
+def run_all_monitoring_checks_sync():
+    asyncio.run(run_all_monitoring_checks())
+
+
+@asynccontextmanager
+async def lifespan(_: FastAPI):
+    for job in scheduler.get_jobs():
+        scheduler.cancel(job)
+
+    scheduler.schedule(scheduled_time=datetime.now(timezone.utc),
+                       func=run_all_monitoring_checks_sync,
+                       interval=settings.TIMEOUT_CHECK_SERVICES,
+                       repeat=None)
+    print("Scheduler started!🚀")
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
 
 api_version_prefix = "/api/v1"
-
+dashboard = RedisQueueDashboard("redis://localhost:6379", "/rq")
+app.mount("/rq", dashboard)
 app.include_router(service_monitoring_router, prefix=api_version_prefix, tags=["service_monitoring"])
-
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
