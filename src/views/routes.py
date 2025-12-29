342fb58ba713f691cca9b064bad1214b52d07ab6
xborismenx <boris.tikhonov.21@gmail.com>
2025-12-29 22:43:00 +0200

    Integrate Redis caching into health check API to optimize service status retrieval and storage

--- src/views/routes.py
+++ src/views/routes.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 from database import get_db
+from database.session_redis import get_redis_cache
 from repository.monitoring_repository import create_service, get_service
 from schemas.monitoring import CreateServiceMonitoringSchema, CreateServiceMonitoringResponseSchema, \
     HealthServiceMonitoringSchema
+from services.cache_service import CacheCircuitBreakerService
 
 router = APIRouter()
 
@@ -15,9 +17,18 @@ async def register_service(new_service: CreateServiceMonitoringSchema, db: Async
 
 
 @router.get("/health/{service_id}/", response_model=HealthServiceMonitoringSchema)
-async def health_service(service_id: int, db: AsyncSession = Depends(get_db)):
+async def health_service(
+        service_id: int,
+        db: AsyncSession = Depends(get_db),
+        redis_cache: CacheCircuitBreakerService = Depends(get_redis_cache)
+):
+    cached_status = await redis_cache.get_service_status(service_id)
+    if cached_status:
+        return cached_status
+
     service = await get_service(service_id, db)
     if not service:
         raise HTTPException(status_code=404, detail="Service not found")
-    return service
 
+    await redis_cache.set_service_status(service_id=service_id, service_data=service)
+    return service
