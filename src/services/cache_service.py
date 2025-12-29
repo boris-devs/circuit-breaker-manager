aa3bbf1fb39e82b3e580730b2d4f6786ea7ba4f3
xborismenx <boris.tikhonov.21@gmail.com>
2025-12-29 22:41:04 +0200

    Add `CacheCircuitBreakerService` to manage service status caching with Redis

---
+++ src/services/cache_service.py
@@ -0,0 +1,20 @@
+import json
+
+from redis.asyncio import Redis
+
+from database.models.circuit_breaker import MonitoredServices
+from schemas.monitoring import HealthServiceMonitoringSchema
+
+
+class CacheCircuitBreakerService:
+    def __init__(self, redis_client: Redis):
+        self.redis_client = redis_client
+
+    async def get_service_status(self, service_id: int) -> dict | None:
+        service = await self.redis_client.get(f"service_status:{service_id}")
+        return json.loads(service) if service else None
+
+    async def set_service_status(self, service_id: int, service_data: MonitoredServices):
+        schema_data = HealthServiceMonitoringSchema.model_validate(service_data)
+        json_data = schema_data.model_dump_json()
+        await self.redis_client.set(f"service_status:{service_id}", json_data, ex=30)
