80fbe08708b1427cb9c4e77cd93b026d0c4833b8
xborismenx <boris.tikhonov.21@gmail.com>
2026-01-03 00:00:10 +0200

    Refactor Redis client handling in monitoring tasks and integrate `redis_pubsub_manager` for improved real-time notifications

--- src/tasks/tasks.py
+++ src/tasks/tasks.py
@@ -1,17 +1,25 @@
 from datetime import timedelta, timezone, datetime
 
-from redis.asyncio import Redis
 from sqlalchemy import select, NullPool, or_
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
-
+from redis.asyncio import Redis as AsyncRedis
 from config.settings import Settings
 from database.models.circuit_breaker import MonitoredServices
 from database.session_postgresql import POSTGRESQL_DATABASE_URL
 from services.cache_service import CacheCircuitBreakerService
+from services.redis_pubsub_manager import redis_pubsub_manager
 from sheduler.rq_sheduler import queue
 from tasks.monitoring import check_service_availability
 
 settings = Settings()
+
+
+async def run_monitoring_and_notify_job():
+    redis_client = AsyncRedis(host=settings.REDIS_HOST, port=settings.REDIS_PORT, decode_responses=True)
+
+    await run_all_monitoring_checks()
+    redis_pubsub_manager.redis_client = redis_client
+    await redis_pubsub_manager.publish()
 
 
 async def run_all_monitoring_checks():
@@ -29,7 +37,6 @@ async def run_all_monitoring_checks():
                                                                             MonitoredServices.last_check < time_distance)))
 
             for service in services.scalars():
-                print(f"Checking service {service.name}")
                 queue.enqueue(check_service_availability_task, service.id)
     finally:
         await postgresql_engine.dispose()
@@ -43,12 +50,13 @@ async def check_service_availability_task(service_id: int):
         expire_on_commit=False,
         autocommit=False,
     )
-    client = Redis(host=settings.REDIS_HOST, port=settings.REDIS_PORT, decode_responses=True)
-    cache_redis = CacheCircuitBreakerService(client)
+    redis_client = AsyncRedis(host=settings.REDIS_HOST, port=settings.REDIS_PORT, decode_responses=True)
+    redis_cache_service = CacheCircuitBreakerService(redis_client)
     try:
         async with async_postgresql_session() as db:
             service = await db.get(MonitoredServices, service_id)
             if service:
-                await check_service_availability(service, db, cache_redis)
+                await check_service_availability(service, db, redis_cache_service)
     finally:
+        await redis_client.close()
         await postgresql_engine.dispose()
