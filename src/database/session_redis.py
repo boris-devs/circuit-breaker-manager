d54c3888bee6b12c1eb0a757c4f6b2709f776684
xborismenx <boris.tikhonov.21@gmail.com>
2026-01-02 23:57:44 +0200

    Refactor Redis client initialization to use a factory function for improved maintainability and clarity

--- src/database/session_redis.py
+++ src/database/session_redis.py
@@ -4,9 +4,13 @@ from config.settings import Settings
 from services.cache_service import CacheCircuitBreakerService
 
 settings = Settings()
+
 redis_client_sync = Redis(host=settings.REDIS_HOST, port=settings.REDIS_PORT)
-redis_client_async = AsyncRedis(host=settings.REDIS_HOST, port=settings.REDIS_PORT, decode_responses=True)
+
+
+def get_async_redis() -> AsyncRedis:
+    return AsyncRedis(host=settings.REDIS_HOST, port=settings.REDIS_PORT, decode_responses=True)
 
 
 async def get_redis_cache():
-    return CacheCircuitBreakerService(redis_client_async)
+    return CacheCircuitBreakerService(get_async_redis())
