b194b822c254504a65a238be47c9cab4d811fadc
xborismenx <boris.tikhonov.21@gmail.com>
2026-01-03 00:00:42 +0200

    Refactor application initialization to include Prometheus metrics and Redis Pub/Sub for real-time monitoring

--- main.py
+++ main.py
@@ -1,39 +1,52 @@
 import asyncio
 from datetime import datetime, timezone
 from contextlib import asynccontextmanager
-
 from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
 from rq_dashboard_fast import RedisQueueDashboard
 
 from config.settings import Settings
-from tasks.tasks import run_all_monitoring_checks
+from database.session_redis import get_async_redis
+from services.redis_pubsub_manager import redis_pubsub_manager
+from services.websocket_manager import ws_manager
+from tasks.tasks import run_monitoring_and_notify_job
 from views import service_monitoring_router
-from sheduler.rq_sheduler import scheduler, queue
+from sheduler.rq_sheduler import scheduler
 
 settings = Settings()
 
+app = FastAPI()
 
-def run_all_monitoring_checks_sync():
-    asyncio.run(run_all_monitoring_checks())
+instrumentator = Instrumentator().instrument(app)
 
 
 @asynccontextmanager
 async def lifespan(_: FastAPI):
+    instrumentator.expose(app)
+
+    redis_pubsub_manager.redis_client = get_async_redis()
+    scheduler.schedule(scheduled_time=datetime.now(timezone.utc),
+                       func=run_monitoring_and_notify_job,
+                       interval=15,
+                       result_ttl=30,
+                       repeat=None)
+    print("Scheduler started!🚀")
+
+    pubsub_task = asyncio.create_task(
+        redis_pubsub_manager.start_listener(ws_manager)
+    )
+
+    yield
+
+    pubsub_task.cancel()
     for job in scheduler.get_jobs():
         scheduler.cancel(job)
 
-    scheduler.schedule(scheduled_time=datetime.now(timezone.utc),
-                       func=run_all_monitoring_checks_sync,
-                       interval=15,
-                       result_ttl=120,
-                       repeat=None)
-    print("Scheduler started!🚀")
-    yield
 
-
-app = FastAPI(lifespan=lifespan)
+app.router.lifespan_context = lifespan
 
 api_version_prefix = "/api/v1"
+
 dashboard = RedisQueueDashboard("redis://localhost:6379", "/rq")
 app.mount("/rq", dashboard)
 app.include_router(service_monitoring_router, prefix=api_version_prefix, tags=["service_monitoring"])
