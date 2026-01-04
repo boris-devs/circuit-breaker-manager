d649e47db63689fce833c625b66d4e31b01c2d75
xborismenx <boris.tikhonov.21@gmail.com>
2026-01-04 20:02:48 +0200

    Refactor PostgreSQL session initialization to use settings-based sync and async connection URLs

--- src/database/session_postgresql.py
+++ src/database/session_postgresql.py
@@ -7,20 +7,17 @@ from config.settings import Settings
 
 settings = Settings()
 
-POSTGRESQL_DATABASE_URL = (f"postgresql+asyncpg://{settings.POSTGRES_USER}:{settings.POSTGRES_PASSWORD}@"
-                           f"{settings.POSTGRES_HOST}:{settings.POSTGRES_DB_PORT}/{settings.POSTGRES_DB}")
+sync_postgresql_engine = create_engine(url=settings.sync_postgresql_url, echo=False)
 
-postgresql_engine = create_async_engine(url=POSTGRESQL_DATABASE_URL, echo=False)
+async_postgresql_engine = create_async_engine(url=settings.async_postgresql_url, echo=False)
 
 AsyncPostgresSessionLocal = async_sessionmaker(
-    bind=postgresql_engine,
+    bind=async_postgresql_engine,
     class_=AsyncSession,
     autocommit=False,
     expire_on_commit=False,
 
 )
-sync_postgresql_engine = POSTGRESQL_DATABASE_URL.replace("postgresql+asyncpg", "postgresql")
-SyncPostgresSessionLocal = create_engine(url=sync_postgresql_engine, echo=False)
 
 
 async def get_postgresql_db() -> AsyncGenerator[AsyncSession, None]:
