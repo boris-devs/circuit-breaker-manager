919c0762c492d505e850e8587c84a95c1c20b42b
xborismenx <boris.tikhonov.21@gmail.com>
2026-01-05 00:19:31 +0200

    Remove unused `Settings` import from `rq_sheduler.py`

--- src/sheduler/rq_sheduler.py
+++ src/sheduler/rq_sheduler.py
@@ -2,9 +2,7 @@
 from rq import Queue
 from rq_scheduler import Scheduler
 from database.session_redis import redis_client_sync
-from config.settings import Settings
 
-settings = Settings()
 
 queue = Queue(connection=redis_client_sync)
 scheduler = Scheduler(queue=queue, connection=redis_client_sync)
